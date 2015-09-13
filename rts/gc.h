4d95d9704aa6574f6459242c79b8d8d8de2ea2f2
Edward Kmett <ekmett@gmail.com>
2015-09-13 05:20:07 -0400

    teach gc how to register cores

--- rts/gc.h
+++ rts/gc.h
@@ -1,9 +1,11 @@
 #ifndef INCLUDED_RTS_GC_H
 #define INCLUDED_RTS_GC_H
 
-#include <boost/thread.hpp>
 #include <cassert>
 #include <cstdint>
+#include <mutex>
+#include <unordered_set>
+#include "thread_local.h"
 
 namespace thc {
 
@@ -30,6 +32,7 @@ enum class types : std::uint64_t {
 };
 
 class core;
+struct gc_ptr;
 
 class gc {
 
@@ -41,40 +44,51 @@ class gc {
   std::uint32_t regions_end;
   std::uint64_t * mapped_regions; // 1 bit per region, packed
 
+  std::mutex mutex_;
+  std::unordered_set<core*> cores;
+
   friend core;
-  void register(core &);
-  void unregister(core &);
+  friend gc_ptr;
 
-  public:
-    gc();  // allocate a growable system heap, add parameters here
-    ~gc(); // return memory to the system
-
-  static inline bool in_protected_region(ptr p) {
-    auto r = p.region;
-    assert(regions_begin <= r && r < regions_end);
-    return p.space && mapped_regions[r>>6]&(1<<((r-regions_begin)&0x3f));
+  static void add_core(core & c) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    cores.insert(c);
+  }
+  void remove_core(core & c) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    cores.erase(c);
+    // TODO: check to make sure we don't need to advance the gc state if we were blocking?
   }
 
-}
+  public:
+    gc() {}  // TODO: allocate a growable system heap, add parameters here
+    ~gc() {} // TODO: return memory to the system
+
+    static inline bool is_protected_region(uint32_t r) {
+      assert(regions_begin <= r && r < regions_end);
+      return mapped_regions[r>>6]&(1<<((r-regions_begin)&0x3f));
+    }
+};
 
 // cores are haskell execution contexts.
 class core {
   private:
-    std::uint16_t expected_nmt; // array of 16 bits
-    gc & system_;
-    static extern boost::thread_specific_ptr<core> current;
-
     core(core const &);               // private copy constructor for RAII
     core & operator = (core const &); // private assignment operator for RAII
 
+    std::uint16_t expected_nmt; // array of 16 bits
+    gc & system_;
+    // static extern boost::thread_specific_ptr<core> current;
+    static thread_local extern core * current;
+
   public:
     core(gc & system) : system_(system) {
-      current.reset(this);
-      system.register(*this);
+      current = this;
+      system_.add_core(*this);
     }
     ~core() {
-      system.unregister(*this);
-      current.release();
+      system_.remove_core(*this);
+      current = nullptr;
     }
 
     // perform raii to bind to the current_core
@@ -85,8 +99,9 @@ class core {
 struct gc_ptr {
 
   union {
-    // layout chosen so that a 0-extended 32 bit integer is a legal 'gc_ptr'
-    // as are legal native c pointers
+    // layout chosen so that a 0-extended 32 bit integer is a legal 'gc_ptr' as are legal native c pointers
+    // TODO: consider setting space 15 to also be a native pointer that way 0 and 1 extended pointers would
+    // be legal.
     std::uintptr_t type     : 3,  // locally unique?
                    offset   : 9,  // offset within a 4k page
                    segment  : 9,  // which 4k page within a 2mb region
@@ -108,11 +123,19 @@ struct gc_ptr {
     return reinterpret_cast<T *>(addr & mask);
   };
 
+  static inline bool is_protected() {
+    auto r = p.region;
+    assert(regions_begin <= r && r < regions_end);
+    return p.space && mapped_regions[r>>6]&(1<<((r-regions_begin)&0x3f));
+  }
+
+  // modify this to look like an assignment operator e.g. operator = (...) ?
+
   inline void lvb(void * address) {
     int trigger = 0;
-    core current = *core::current;
-    if (nmt != current->get_expected_nmt(space)) trigger |= triggers::nmt;
-    if (current->gc.is_protected(region))   trigger |= triggers::reloc;
+    auto c = core::current;
+    if (nmt != c->get_expected_nmt(space))          trigger |= triggers::nmt;
+    if (space && c->gc.in_protected_region(region)) trigger |= triggers::reloc;
     if (trigger) lvb_slow_path(address, trigger)
   }
 
