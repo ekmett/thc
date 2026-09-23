9dba732fd13307940b53ae1c319454a1fb0ffbaf
Edward Kmett <ekmett@gmail.com>
2026-09-23 15:32:04 +0200

    Export exact vector shapes and prepare native Int64X2 oracle

---
+++ compiler/test-fixtures/SimdInt64X2.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE MagicHash, UnboxedTuples #-}
+module SimdInt64X2 where
+import GHC.Exts
+
+-- OPAQUE retains the scalar entry; vectors stay local to this first slice.
+{-# OPAQUE vectorCase #-}
+vectorCase :: Int# -> Int# -> Int#
+vectorCase a b =
+  case packInt64X2# (# intToInt64# a, intToInt64# b #) of
+    x -> case broadcastInt64X2# (intToInt64# (a +# 91#)) of
+      y -> case plusInt64X2# x y of
+        z -> case unpackInt64X2# z of
+          (# p, q #) -> (int64ToInt# p *# 7#) `xorI#` (int64ToInt# q *# 11#)
+
+{-# OPAQUE subtractCase #-}
+subtractCase :: Int# -> Int# -> Int#
+subtractCase a b =
+  case packInt64X2# (# intToInt64# a, intToInt64# b #) of
+    x -> case minusInt64X2# x (negateInt64X2# (broadcastInt64X2# (intToInt64# (b -# 19#)))) of
+      z -> case unpackInt64X2# z of
+        (# p, q #) -> (int64ToInt# p *# 13#) `xorI#` (int64ToInt# q *# 17#)
+
+{-# OPAQUE branchCase #-}
+branchCase :: Int# -> Int# -> Int#
+branchCase a b =
+  case packInt64X2# (# intToInt64# a, intToInt64# b #) of
+    x -> case (case a ># b of 0# -> negateInt64X2# x; _ -> x) of
+      z -> case unpackInt64X2# (plusInt64X2# z x) of
+        (# p, q #) -> (int64ToInt# p *# 23#) `xorI#` (int64ToInt# q *# 31#)
---
+++ compiler/test-fixtures/SimdInt64X2Native.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE MagicHash #-}
+module Main where
+import GHC.Exts
+import SimdInt64X2
+main :: IO ()
+main = mapM_ run [(name, f, a, b) | (name, f) <- entries, a <- inputs, b <- inputs]
+  where
+    entries = [("vectorCase", vectorCase), ("subtractCase", subtractCase), ("branchCase", branchCase)]
+    inputs = [minBound, -3000000001, -1, 0, 1, 9000000003, maxBound] :: [Int]
+    run (name, f, a@(I# x), b@(I# y)) = putStrLn (name ++ "\t" ++ show a ++ "\t" ++ show b ++ "\t" ++ show (I# (f x y)))
