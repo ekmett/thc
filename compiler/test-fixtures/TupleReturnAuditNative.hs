8581eb6da8aa30e5a0fc736e7517c660dbf1600d
Edward Kmett <ekmett@gmail.com>
2026-09-23 14:49:41 +0200

    Add native result-only tuple return oracle and shape checks

---
+++ compiler/test-fixtures/TupleReturnAuditNative.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE MagicHash #-}
+module Main where
+import TupleReturnAudit
+import GHC.Exts (Int(I#), Int#)
+
+entries :: [(String, Int# -> Int#)]
+entries = [("emptyCase", emptyCase), ("singleIntCase", singleIntCase),
+           ("singleBoxCase", singleBoxCase), ("singleClosureCase", singleClosureCase),
+           ("nestedCase", nestedCase), ("unliftedBoxedLeafCase", unliftedBoxedLeafCase),
+           ("selfTailCase", selfTailCase), ("mutualTailCase", mutualTailCase),
+           ("nonTailCase", nonTailCase), ("papCase", papCase),
+           ("overapplicationCase", overapplicationCase)]
+
+inputs :: [Int]
+inputs = [minBound, -4097, -1, 0, 1, 4097, 3000000000, maxBound]
+
+row :: String -> (Int# -> Int#) -> Int -> IO ()
+row name f (I# x) = putStrLn (name ++ "\t" ++ show (I# x) ++ "\t" ++ show (I# (f x)))
+
+main :: IO ()
+main = do
+  mapM_ (\(name, f) -> mapM_ (row name f) inputs) entries
+  mapM_ (row "selfTailDepth" selfTailDepth) [0, 1, 20000]
+  mapM_ (row "mutualTailDepth" mutualTailDepth) [0, 1, 20001]
