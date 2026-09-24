bc88bec991bee22a80ada5f38ff121ae349da7d5
Edward Kmett <ekmett@gmail.com>
2026-09-24 19:36:24 +0200

    Preserve Safe Haskell trust in Core capture

--- test/fixtures/run-store-project/dep-data/src/Answer.hs
+++ test/fixtures/run-store-project/dep-data/src/Answer.hs
@@ -1,8 +1,11 @@
 -- SPDX-FileCopyrightText: 2026 Edward Kmett
 -- SPDX-License-Identifier: UPL-1.0 AND BSD-3-Clause
 
+{-# LANGUAGE Safe #-}
 module Answer (answerValue) where
 
+import SafeDependency (stableValue)
+
 answerValue :: Int
-answerValue = 42
+answerValue = stableValue
 {-# OPAQUE answerValue #-}
