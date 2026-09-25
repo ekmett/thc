884c312d074d44605acebdc6cfbb911596eec399
Edward Kmett <ekmett@gmail.com>
2026-09-25 06:45:29 +0200

    Stop broad CI after the first test failure

--- scripts/try.sh
+++ scripts/try.sh
@@ -8,4 +8,4 @@ cd "$THC_ROOT"
 make --no-print-directory -s -C "$THC_ROOT" check-java
 scripts/prepare-tests.sh
 # Normal dependency downloads are enabled. Pass --offline explicitly if desired.
-./gradlew --no-daemon test installDist "$@"
+./gradlew --no-daemon test "$@" installDist
